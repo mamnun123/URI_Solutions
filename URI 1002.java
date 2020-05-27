efd43f8d0a7b1ecfed4deca3e0b16b30c2cf86c2
Mehedi Hasan <mahathirmehedi@gmail.com>
2020-05-27 15:02:56 +0600

    Area of a Circle
    https://www.urionlinejudge.com.br/judge/en/problems/view/1002

---
+++ URI 1002.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        double rad, area;
+        Scanner input = new Scanner(System.in);
+        rad = input.nextDouble();
+        area = 3.14159 * R * R;
+        System.out.printf("A=%.4f\n",  area;
+   }
+}
---
+++ URI 1002.py
@@ -0,0 +1,3 @@
+r = input()
+area = 3.14159 * (float(r) * float(r))
+print('A={:.4f}'.format(area))
