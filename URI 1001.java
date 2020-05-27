a9011d8bf75fe245d424d5d375b9943b5aa1c004
Mehedi Hasan <mahathirmehedi@gmail.com>
2020-05-27 14:58:29 +0600

    Extremely Basic
    https://www.urionlinejudge.com.br/judge/en/problems/view/1001

---
+++ URI 1001.java
@@ -0,0 +1,16 @@
+package URI_1001_;
+
+import java.util.Scanner;
+
+public class URI_1001 {
+
+	public static void main(String[] args) 
+	{
+		int A, B, X;
+		Scanner input = new Scanner(System.in);
+		A = input.nextInt();
+		B = input.nextInt();
+		X = A + B;
+		System.out.print(X+"\n");
+	}
+}
